5f9a25690b2884270f2024b3aa5b91b92ebf7a64
SamridKC <samrid95@gmail.com>
2018-02-20 12:29:00 -0700

    add auth.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var http = require('http');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var authController = require('./auth');
 
 var app = express();
 app.use(bodyParser.text( {
@@ -9,14 +11,22 @@ app.use(bodyParser.text( {
     }
 }));
 
-app.post('/post', function (req, res) {
+app.use(passport.initialize());
+
+var router = express.Router();
+
+router.route('/post')
+//app.post('/post', function (req, res) {
+    .post(authController.isAuthenticated, function (req, res) {
     console.log(req.body);
     res = res.status(200);
     if (req.get('Content-Type')) {
         console.log("Content-Type: " + req.get('Content-Type'));
         res = res.type(req.get('Content-Type'));
-    }
+        }
     res.send(req.body);
-});
+    }
+);
 
-http.createServer(app).listen(process.env.PORT || 8080);
+app.use('/', router);
+app.listen(process.env.PORT || 8080);
